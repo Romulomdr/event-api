b7505ac4bad2de94fdb8428d4dfe8345846abba8
Romulomdr <romulomatheus31@gmail.com>
2024-07-12 18:10:42 -0300

    first commit

---
+++ src/main/java/com/eventapi/event/domain/coupon/Coupon.java
@@ -0,0 +1,39 @@
+package com.eventapi.event.domain.coupon;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.eventapi.event.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name="coupon")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+	
+	@Id
+	@GeneratedValue
+	private UUID id;
+	
+	private String code;
+	private Integer discount;
+	private Date date;
+	
+	
+	@ManyToOne
+	@JoinColumn(name="event_id")
+	private Event event;
+}
